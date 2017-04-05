570a7e1c97317bc73fb1761ee0e034306ad3a372
martsan <martsan324@gmail.com>
2017-04-05 23:18:55 +0200

    Adding models,Controller + CRUD

---
+++ SmartCardReader/SmartCardReader.WebUI/Controllers/SubjectsController.cs
@@ -0,0 +1,55 @@
+﻿using SmartCardReader.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmartCardReader.WebUI.Controllers
+{
+
+    public class SubjectsController : Controller
+    {
+        private SubjectDBCtxt baza = new SubjectDBCtxt();
+        // GET: Subjects
+        public ActionResult Index()
+        {
+
+
+            return View(baza.Subjects.ToList());
+        }
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create([Bind(Include = "Id,TeaTeacher_ID,Teacher_Name,Teacher_Surname,Subject_Name")] Subject subject)
+        {
+
+            if (ModelState.IsValid)
+            {
+
+                baza.Subjects.Add(subject);
+
+                baza.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(subject);
+        }
+
+        public ActionResult Remove(int id)
+        {
+            var temp = baza.Subjects.Find(id);
+            baza.Subjects.Remove(temp);
+            baza.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ SmartCardReader/SmartCardReader.WebUI/Models/Subject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace SmartCardReader.WebUI.Models
+{
+    public class Subject
+    {
+        public int Id { get; set; }
+        public int Teacher_ID { get; set; }
+        public string Teacher_Name { get; set; }
+        public string Teacher_Surname { get; set; }
+        public string Subject_Name { get; set; }
+
+    }
+    public class SubjectDBCtxt : DbContext
+    {
+        public DbSet<Subject> Subjects { get; set; }
+    }
+
+}
