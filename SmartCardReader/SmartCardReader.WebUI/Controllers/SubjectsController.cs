7232ec85ea19447cde6ee3ee196ab97518f8c82c
martsan <martsan324@gmail.com>
2017-04-19 21:39:00 +0200

    Refactor

--- SmartCardReader/SmartCardReader.WebUI/Controllers/SubjectsController.cs
+++ SmartCardReader/SmartCardReader.WebUI/Controllers/SubjectsController.cs
@@ -1,6 +1,7 @@
 ﻿using SmartCardReader.WebUI.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -44,6 +45,49 @@ namespace SmartCardReader.WebUI.Controllers
             return View(subject);
         }
 
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Subject subject = baza.Subjects.Find(id);
+            if (subject == null)
+            {
+                return HttpNotFound();
+            }
+            return View(subject);
+        }
+
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Subject subject = baza.Subjects.Find(id);
+            if (subject == null)
+            {
+                return HttpNotFound();
+            }
+            return View(subject);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,TeaTeacher_ID,Teacher_Name,Teacher_Surname,Subject_Name")] Subject subject)
+        {
+            if (ModelState.IsValid)
+            {
+                baza.Entry(subject).State = EntityState.Modified;
+                baza.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(subject);
+        }
+
         public ActionResult Remove(int id)
         {
             var temp = baza.Subjects.Find(id);
@@ -51,5 +95,8 @@ namespace SmartCardReader.WebUI.Controllers
             baza.SaveChanges();
             return RedirectToAction("Index");
         }
+
+
+   
     }
 }
