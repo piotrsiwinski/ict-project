905b4127b8641765dd96777f9fe6a3a2b272cc09
Piotr <siwinski.piotr@outlook.com>
2017-04-20 11:03:47 +0200

    Added JavaFx project

---
+++ SmartCardFX/src/sample/Controller.java
@@ -0,0 +1,19 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+
+public class Controller {
+    @FXML
+    public Button btn;
+
+
+    public Controller() {
+    }
+
+    public void onClick(ActionEvent actionEvent) {
+
+    }
+}
